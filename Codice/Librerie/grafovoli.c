019f47c40b1cdf6df7a07d40f96564eba8b8e767
aleks-nikolov <a.nikolov@studenti.unina.it>
2020-06-01 18:29:21 +0200

    Aggiunti file sui grafi e l'algoritmo di Dijkstra

---
+++ Codice/Librerie/grafovoli.c
@@ -0,0 +1,207 @@
+#include "grafovoli.h"
+
+//Inizializza gli aeroporti con alcune tratte predefinite (per ora sono 10 per semplicità di testing, poi li incremento)
+t_grf * inizializzaGrafo(t_grf * g) {
+    g = aggiungiAeroporto(g, "NAPOLI");
+    g = aggiungiAeroporto(g, "ROMA");
+    g = aggiungiAeroporto(g, "MILANO");
+    g = aggiungiAeroporto(g, "LONDRA");
+    g = aggiungiAeroporto(g, "PARIGI");
+    g = aggiungiAeroporto(g, "SCHIPOL");
+    g = aggiungiAeroporto(g, "FRANCOFORTE");
+    g = aggiungiAeroporto(g, "MOSCA");
+    g = aggiungiAeroporto(g, "MADRID");
+    g = aggiungiAeroporto(g, "BARCELLONA");
+
+    g = aggiungiVolo(g, "NAPOLI", "ROMA", 50.00f, 45);
+    g = aggiungiVolo(g, "ROMA", "MILANO", 65.00f, 50);
+    g = aggiungiVolo(g, "MILANO", "NAPOLI", 95.00f, 100);
+    g = aggiungiVolo(g, "ROMA", "LONDRA", 110.00f, 180);
+    g = aggiungiVolo(g, "ROMA", "SCHIPOL", 95.00f, 145);
+    g = aggiungiVolo(g, "ROMA", "MADRID", 80.00f, 120);
+    g = aggiungiVolo(g, "MADRID", "BARCELLONA", 40, 30);
+    g = aggiungiVolo(g, "SCHIPOL", "MOSCA", 120.00f, 160);
+
+    return g;
+}
+
+//Aggiunge un nuovo aeroporto in coda alla lista dei vertici
+t_grf * aggiungiAeroporto(t_grf * g, char *nome) {
+    if(grafoVuoto(g)){
+        g = (t_grf *)malloc(sizeof(t_grf));
+        strcpy(g->nome, nome);
+        g->archi = NULL;
+        g->next = NULL;
+    } else{
+        g->next = aggiungiAeroporto(g->next, nome);
+    }
+
+    return g;
+}
+
+//Aggiunge un nuovo aeroporto in testa alla lista dei vertici (usato solo negli algoritmi di Dijkstra)
+t_grf * aggiungiAeroportoInTesta(t_grf * g, char *nome) {
+    t_grf * temp = (t_grf *)malloc(sizeof(t_grf));
+    strcpy(temp->nome, nome);
+    if(!grafoVuoto(g)) {
+        temp->next = g;
+        return temp;
+    } else {
+        temp->next = NULL;
+        return temp;
+    }
+}
+
+//Elimina l'aeroporto di nome 'nome' dal grafo 'g'
+t_grf * eliminaAeroporto(t_grf * g, char *nome) {
+    if(!grafoVuoto(g)){
+        g->next = eliminaAeroporto(g->next, nome);
+        if(strcmp(g->nome, nome) == 0) {
+            t_grf * temp = g->next;
+            free(g);
+            return temp;
+        }
+    }
+
+    return g;
+}
+
+//Aggiunge un nuovo volo da 'uscita' a 'entrata' con il relativo prezzo e durata, effettuando vari controlli
+t_grf * aggiungiVolo(t_grf * g, char *uscita, char *entrata, float prezzo, int durata) {
+    t_grf * temp;
+
+    if(grafoVuoto(g))
+        printf("\nERRORE: Il grafo non risulta inizializzato");
+    else if (strcmp(uscita, entrata) == 0)
+        printf("\nATTENZIONE: Impossibile aggiungere un volo da un aeroporto a se stesso");
+    else if (!aeroportoEsistente(g, uscita) || !aeroportoEsistente(g, entrata))
+        printf("\nATTENZIONE: Almeno uno degli aeroporti inseriti non risulta presente");
+    else if (voloEsistente(getVertice(g, uscita)->archi, entrata)) {
+        printf("\nATTENZIONE: Il volo che si tenta di inserire esiste gia'");
+    } else {
+        temp = getVertice(g, uscita);
+        temp->archi = inizializzaArco(temp->archi, entrata, prezzo, durata);
+    }
+
+    return g;
+}
+
+//Elimina il volo da 'uscita' a 'entrata', effettuando vari controlli
+t_grf * eliminaVolo(t_grf * g, char *uscita, char *entrata) {
+    t_grf * temp;
+
+    if(grafoVuoto(g))
+        printf("\nERRORE: Il grafo non risulta inizializzato");
+    else if (!aeroportoEsistente(g, uscita) || !aeroportoEsistente(g, entrata))
+        printf("\nATTENZIONE: Almeno uno degli aeroporti inseriti non risulta presente");
+    else if (voloEsistente(getVertice(g, uscita)->archi, entrata)) {
+        printf("\nATTENZIONE: Il volo che si tenta di eliminare non esiste");
+    } else {
+        temp = getVertice(g, uscita);
+        temp->archi = eliminaArco(temp->archi, entrata);
+    }
+
+    return g;
+}
+
+//Inizializza un nuovo arco e lo restituisce (usata dalla funzione aggiungiVolo)
+t_arc * inizializzaArco(t_arc * a, char *nome, float prezzo, int durata) {
+    if(arcoVuoto(a)) {
+        a = (t_arc *) malloc(sizeof(t_arc));
+        strcpy(a->nome, nome);
+        a->prezzo = prezzo;
+        a->durata = durata;
+        a->next = NULL;
+        return a;
+    } else {
+        a->next = inizializzaArco(a->next, nome, prezzo, durata);
+    }
+
+    return a;
+}
+
+//Dealloca un arco, usata dalla funzione eliminaVolo
+t_arc * eliminaArco(t_arc * a, char *nome) {
+    if(!arcoVuoto(a)){
+        a->next = eliminaArco(a->next, nome);
+        if(strcmp(a->nome, nome) == 0) {
+            t_arc * temp = a->next;
+            free(a);
+            return temp;
+        }
+    }
+
+    return a;
+}
+
+//Elimina tutti i voli verso un aeroporto, usata nel caso di eliminazione di un aeroporto dal grafo
+t_grf * eliminaTuttiVoliAdAeroporto(t_grf * g, char *nome) {
+    if(!grafoVuoto(g)) {
+        g->archi = eliminaArco(g->archi, nome);
+        g->next = eliminaTuttiVoliAdAeroporto(g->next, nome);
+    }
+
+    return g;
+}
+
+//Restituisce il puntatore al vertice con nome 'nome'
+t_grf * getVertice(t_grf * g, char *nome) {
+    if(strcmp(g->nome, nome) == 0)
+        return g;
+    else
+        return getVertice(g->next, nome);
+}
+
+//Controlla se esiste il volo dal campo 'archi' di un vertice a 'entrata'
+int voloEsistente(t_arc * a, char *entrata) {
+    if(!arcoVuoto(a)) {
+        if(strcmp(a->nome, entrata) == 0)
+            return 1;
+        else
+            return voloEsistente(a->next, entrata);
+    }
+
+    return 0;
+}
+
+//Controlla l'esistenza di un aeroporto nel grafo
+int aeroportoEsistente(t_grf * g, char *nome) {
+    if(!grafoVuoto(g)) {
+        if(strcmp(g->nome, nome) == 0)
+            return 1;
+        else
+            return aeroportoEsistente(g->next, nome);
+    }
+    return 0;
+}
+
+//Restituisce la dimensione del grafo
+int lunghezzaGrafo(t_grf * g) {
+    if(grafoVuoto(g))
+        return 0;
+    else
+        return 1 + lunghezzaGrafo(g->next);
+}
+
+void stampaGrafo(t_grf * G) {
+    if(!grafoVuoto(G)){
+        printf("\n%s ", G->nome);
+        stampaArchi(G->archi);
+        stampaGrafo(G->next);
+    }
+}
+
+void stampaArchi(t_arc * a) {
+    if(!arcoVuoto(a)) {
+        printf("--(%.2f euro)(%d min)--> %s ", a->prezzo, a->durata, a->nome);
+        stampaArchi(a->next);
+    }
+}
+
+int grafoVuoto(t_grf * g) {
+    return (g == NULL);
+}
+
+int arcoVuoto(t_arc * a) {
+    return (a == NULL);
+}
---
+++ Codice/Librerie/grafovoli.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LUNGHEZZA_NOME_AEROPORTO 100
+
+typedef struct arco{
+    char nome[LUNGHEZZA_NOME_AEROPORTO];    //nome dell'aeroporto a cui punta l'arco
+
+    float prezzo;     //peso prezzo dell'arco
+    int durata;       //peso durata dell'arco
+
+    struct arco *next;
+} t_arc;
+
+
+typedef struct grafo {
+    char nome[LUNGHEZZA_NOME_AEROPORTO];    //nome dell'aeroporto
+
+    struct arco *archi;
+    struct grafo *next;
+
+    int potenziale;       //il peso totale del cammino
+    struct grafo *prev;   //puntatore al vertice che precede nel cammino minimo
+} t_grf;
+
+//NOTA: le implementazioni delle funzioni sono divise nei file grafovoli.c e dijkstra.c per fini organizzativi
+//Operazioni sui vertici
+t_grf * inizializzaGrafo(t_grf * g);
+t_grf * aggiungiAeroporto(t_grf * g, char *nome);
+t_grf * aggiungiAeroportoInTesta(t_grf * g, char *nome);
+t_grf * eliminaAeroporto(t_grf * g, char *nome);
+
+//Operazioni sugli archi
+t_arc * inizializzaArco(t_arc * a, char *nome, float prezzo, int durata);
+t_grf * aggiungiVolo(t_grf * g, char *uscita, char *entrata, float prezzo, int durata);
+t_grf * eliminaVolo(t_grf * g, char *uscita, char *entrata);
+t_arc * eliminaArco(t_arc * a, char *nome);
+t_grf * eliminaTuttiVoliAdAeroporto(t_grf * g, char *nome);
+
+//Stampe
+void stampaGrafo(t_grf * G);
+void stampaArchi(t_arc * a);
+
+//Utilità
+t_grf * getVertice(t_grf * g, char *nome);
+int voloEsistente(t_arc * a, char *entrata);
+int aeroportoEsistente(t_grf * g, char *nome);
+int lunghezzaGrafo(t_grf * g);
+int grafoVuoto(t_grf * g);
+int arcoVuoto(t_arc * a);
+
+//Dijkstra
+t_grf * dijkstra(t_grf * g, char nome[], int pesoDiRiferimento);
+t_grf * copiaGrafo(t_grf * g, t_grf * q);
+t_grf * impostaPotenziale(t_grf * q, char *nome, int potenziale);
+t_grf * impostaPrecedente(t_grf * q, char *nome, t_grf * precedente);
+t_grf * minimoPotenziale(t_grf * q);
