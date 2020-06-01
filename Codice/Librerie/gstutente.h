8c8be616cfd4574bef6eb3fc223fe6fbff57bf73
lorenzo-uni <lor.lama@studenti.unina.it>
2020-06-01 16:33:36 +0200

    Update gstutente.h

--- Codice/Librerie/gstutente.h
+++ Codice/Librerie/gstutente.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "gstabr.h"
-//#include "gstgrafi.h"
+//#include "abr.h"
+//#include "grafi.h"
 
 void toUpper_String(char * string);
