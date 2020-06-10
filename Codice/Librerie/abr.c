980865692f3c9712f08bc7fa889b6859e63db3da
lorenzo-uni <lor.lama@studenti.unina.it>
2020-06-10 18:21:53 +0200

    Inizializzati 3 clienti(invece di 2) con dei voli

--- Codice/Librerie/abr.c
+++ Codice/Librerie/abr.c
@@ -62,47 +62,65 @@ void mostraUtenti(t_abr * radice){
 	if(radice){
 		mostraUtenti(radice->sinistro);
 		if(radice->utente.admin == 1)
-			printf("%s (admin), password: %s\n", radice->utente.nomeUtente, radice->utente.password);
+			printf("%s, password: %s (admin)\n", radice->utente.nomeUtente, radice->utente.password);
 		else
-			printf("%s (cliente), password: %s\n", radice->utente.nomeUtente, radice->utente.password);
+			printf("%s, password: %s (cliente)\n", radice->utente.nomeUtente, radice->utente.password);
 		mostraUtenti(radice->destro);
 	}
 }
 
-t_abr * inizializzaUtenti(){//metodo lanciato a inizio programma per inizializzare 3 utenti admin e 2 utenti clienti a cui diamo anche delle prenotazioni già effettuate
+t_abr * inizializzaUtenti(){//metodo lanciato a inizio programma per inizializzare 3 utenti admin e 3 utenti clienti a cui diamo anche delle prenotazioni già effettuate
 	t_abr * tmp = NULL;
 	
 	t_lista_S * scaliC1_1 = NULL;
 	t_lista_S * scaliC1_2 = NULL;
+	t_lista_S * scaliC1_3 = NULL;
 	
 	t_lista_S * scaliC2_1 = NULL;
 	t_lista_S * scaliC2_2 = NULL;
 	
+	t_lista_S * scaliC3_1 = NULL;
+	t_lista_S * scaliC3_2 = NULL;
+	t_lista_S * scaliC3_4 = NULL;
+	
 	scaliC1_1 = inserisciInCoda_S(scaliC1_1, "ROMA");
-	scaliC1_2 = inserisciInCoda_S(scaliC1_2, "BARCELLONA");
+	scaliC1_2 = inserisciInCoda_S(scaliC1_2, "ROMA");
 	scaliC1_2 = inserisciInCoda_S(scaliC1_2, "MILANO");
-	scaliC1_2 = inserisciInCoda_S(scaliC1_2, "FRANCOFORTE");
+	scaliC1_2 = inserisciInCoda_S(scaliC1_2, "GINEVRA");
 	
+	scaliC2_1 = inserisciInCoda_S(scaliC2_1, "ROMA");
 	scaliC2_1 = inserisciInCoda_S(scaliC2_1, "MILANO");
-	scaliC2_1 = inserisciInCoda_S(scaliC2_1, "FRANCOFORTE");
-	scaliC2_2 = inserisciInCoda_S(scaliC2_2, "BARCELLONA");
-	scaliC2_2 = inserisciInCoda_S(scaliC2_2, "MILANO");
+	scaliC2_2 = inserisciInCoda_S(scaliC2_2, "LONDRA");
+	scaliC2_2 = inserisciInCoda_S(scaliC2_2, "BRUXELLES");
 	
+	scaliC3_1 = inserisciInCoda_S(scaliC3_1, "FRANCOFORTE");
+	scaliC3_2 = inserisciInCoda_S(scaliC3_2, "STOCCOLMA");
+	scaliC3_2 = inserisciInCoda_S(scaliC3_2, "LONDRA");
+	scaliC3_2 = inserisciInCoda_S(scaliC3_2, "BRUXELLES");
+	scaliC3_2 = inserisciInCoda_S(scaliC3_2, "PARIGI");
+	scaliC3_4 = inserisciInCoda_S(scaliC3_4, "NAPOLI");
 	
 	tmp = inserisciNodo_U(tmp, "cliente1", "passwordC1", 0);
 	tmp->utente.prenotazioni = inserisciInCoda_P(tmp->utente.prenotazioni, "NAPOLI", "MILANO", scaliC1_1, 115, 95);
-	tmp->utente.prenotazioni = inserisciInCoda_P(tmp->utente.prenotazioni, "NAPOLI", "MOSCA", scaliC1_2, 285, 245);
-	tmp->utente.punti = 400;
+	tmp->utente.prenotazioni = inserisciInCoda_P(tmp->utente.prenotazioni, "NAPOLI", "PARIGI", scaliC1_2, 215, 145);
+	tmp->utente.prenotazioni = inserisciInCoda_P(tmp->utente.prenotazioni, "PALERMO", "MALTA", NULL, 30, 35);
+	tmp->utente.punti = 360;
 	
 	tmp = inserisciNodo_U(tmp, "cliente2", "passwordC2", 0);
-	tmp->destro->utente.prenotazioni = inserisciInCoda_P(tmp->destro->utente.prenotazioni, "ROMA", "MOSCA", scaliC2_1, 235, 220);
-	tmp->destro->utente.prenotazioni = inserisciInCoda_P(tmp->destro->utente.prenotazioni, "MADRID", "SCHIPOL", scaliC2_2, 170, 190);
-	tmp->destro->utente.punti = 405;
+	tmp->destro->utente.prenotazioni = inserisciInCoda_P(tmp->destro->utente.prenotazioni, "ATENE", "SOFIA", scaliC2_1, 255, 215);
+	tmp->destro->utente.prenotazioni = inserisciInCoda_P(tmp->destro->utente.prenotazioni, "STOCCOLMA", "PARIGI", scaliC2_2, 135, 110);
+	tmp->destro->utente.punti = 390;
+	
+	tmp = inserisciNodo_U(tmp, "cliente3", "passwordC3", 0);
+	tmp->destro->destro->utente.prenotazioni = inserisciInCoda_P(tmp->destro->destro->utente.prenotazioni, "SCHIPOL", "SOFIA", scaliC3_1, 150, 195);
+	tmp->destro->destro->utente.prenotazioni = inserisciInCoda_P(tmp->destro->destro->utente.prenotazioni, "MOSCA", "MADRID", scaliC3_2, 170, 190);
+	tmp->destro->destro->utente.prenotazioni = inserisciInCoda_P(tmp->destro->destro->utente.prenotazioni, "COPENHAGEN", "LONDRA", NULL, 80, 65);
+	tmp->destro->destro->utente.prenotazioni = inserisciInCoda_P(tmp->destro->destro->utente.prenotazioni, "BARCELLONA", "MALTA", scaliC3_4, 170, 145);
+	tmp->destro->destro->utente.punti = 570;
 	
 	tmp = inserisciNodo_U(tmp,"admin1", "passwordA1", 1);
 	tmp = inserisciNodo_U(tmp, "admin2", "passwordA2", 1);
 	tmp = inserisciNodo_U(tmp, "admin3", "passwordA3", 1);
-
 	
 	return tmp;
 }
